8e64fff237cfd229c6914c61f4cafea59950838e
Joanna Högberg <joannahogberg@users.noreply.github.com>
2017-04-11 18:02:31 +0200

    Update aboutus.php

--- aboutus.php
+++ aboutus.php
@@ -2,7 +2,8 @@
 <?php include_once 'partials/header.php'; ?> 
 
   <header class="container-fluid text-center m-5">
-    <h1 class="text-muted">🏆 About Us! 🏆</h1>    
+    <h1 class="text-muted">🏆 About Us! 🏆</h1>  
+    <h6 class="text-muted"><?php include 'partials/formatDate.php'; ?></h6>
 
   </header>
 
