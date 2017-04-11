f7ae0ec7d03dc0e58b79fe50c3c61adc0de35547
Joanna Högberg <joannahogberg@users.noreply.github.com>
2017-04-11 18:02:00 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 
 <?php include_once 'partials/header.php'; ?>
   <header class="container-fluid text-center m-5">
-    <h1 class="text-muted">🏆 Noble Prize Winners! 🏆</h1>    
+    <h1 class="text-muted">🏆 Noble Prize Winners! 🏆</h1>
+    <h6 class="text-muted"><?php include 'partials/formatDate.php'; ?></h6>
   </header>
 
     <?php
