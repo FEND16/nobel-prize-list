391e893fc42cb1fe6fd2538a7c8ec03bababf4d3
Joanna Högberg <joannahogberg@users.noreply.github.com>
2017-04-12 09:44:47 +0200

    Update contact.php

--- contact.php
+++ contact.php
@@ -9,14 +9,6 @@
 
 <h1>
 
-  <script>
-    //Danger zone, here take this shield to protect yourself: 🛡
-    (function titleScroller(text) {
-      document.title = text;
-      setTimeout(function () {
-        titleScroller(text.substr(1) + text.substr(0, 1));
-      }, 100);
-    }(" GET YOUR NOBEL PRIZE WINNERS! "));
-  </script>
+
 
 <?php include_once 'partials/footer.php'; ?>    
