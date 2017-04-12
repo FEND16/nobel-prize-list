232628552bb768065753962dcf27ca0dac568e15
Joanna Högberg <joannahogberg@users.noreply.github.com>
2017-04-12 09:43:55 +0200

    Update index.php

--- index.php
+++ index.php
@@ -22,15 +22,6 @@
 </div>
 </div>
 
-  <script>
-    //Danger zone, here take this shield to protect yourself: 🛡
-    (function titleScroller(text) {
-      document.title = text;
-      setTimeout(function () {
-        titleScroller(text.substr(1) + text.substr(0, 1));
-      }, 100);
-    }(" GET YOUR NOBEL PRIZE WINNERS! "));
-  </script>
 
 <?php include_once 'partials/footer.php'; ?>    
 
