78dc9382684f1467bbd142ece0f34a5267673015
Joanna Högberg <joannahogberg@users.noreply.github.com>
2017-04-12 09:45:11 +0200

    Update footer.php

--- partials/footer.php
+++ partials/footer.php
@@ -3,5 +3,15 @@
 <h6 class="text-center">By Cilla & Joanna</h6>
 </div>
 </footer>
+
+  <script>
+    //Danger zone, here take this shield to protect yourself: 🛡
+    (function titleScroller(text) {
+      document.title = text;
+      setTimeout(function () {
+        titleScroller(text.substr(1) + text.substr(0, 1));
+      }, 100);
+    }(" GET YOUR NOBEL PRIZE WINNERS! "));
+  </script>
 </body>
 </html>
